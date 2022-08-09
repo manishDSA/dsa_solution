b3c8c4e4d2ef9d60611dc40f653538b5a0faaeb4
manishDSA <110934525+manishDSA@users.noreply.github.com>
2022-08-09 12:17:18 -0700

    Update PlusOneLeetCode.java

--- PlusOneLeetCode.java
+++ PlusOneLeetCode.java
@@ -3,18 +3,13 @@ package Leetcoede;
 public class PlusOneLeetCode {
 
 	static int[] Plusone(int[] a) {
-
 		for (int i = 0; i < a.length; i++) {
-
 			if (a[i] != 9) {
 				a[i] = a[i] + 1;
 				break;
 			}
-
 			else {
-
 				a[i] = 0;
-
 			}
 		}
 
@@ -23,10 +18,8 @@ public class PlusOneLeetCode {
 			b[0] = 1;
 			return b;
 		}
-
 		return a;
 	}
-
 	public static void main(String[] args) {
 		int a[] = { 9 };
 
